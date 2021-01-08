38b24a288660ad79a291107d408f22bdf0f767a4
codedbycurtis <69975259+codedbycurtis@users.noreply.github.com>
2021-01-08 11:50:42 +0000

    Add files via upload
    Updated documentation to reflect object functionality and fixed a few smaller issues.

---
+++ MVVM Framework/BaseViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Fires <see cref="PropertyChanged"/> events on demand.
+/// </summary>
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    /// <summary>
+    /// The event that is fired when a property's value changes.
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// Fires a <see cref="PropertyChanged"/> event.
+    /// </summary>
+    /// <param name="propertyName">The name of the property whose value has changed.</param>
+    public void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    /// <summary>
+    /// Sets a property's value.
+    /// </summary>
+    /// <typeparam name="T">The property's type.</typeparam>
+    /// <param name="property">The referenced property's current value.</param>
+    /// <param name="updatedValue">The updated value to set.</param>
+    /// <param name="propertyName">The name of the property that invoked this method.</param>
+    public void SetProperty<T>(ref T property, T updatedValue, [CallerMemberName]string propertyName = "")
+    {
+        if (Equals(property, updatedValue)) { return; }
+        else
+        {
+            property = updatedValue;
+            NotifyPropertyChanged(propertyName);
+        }
+    }
+}
